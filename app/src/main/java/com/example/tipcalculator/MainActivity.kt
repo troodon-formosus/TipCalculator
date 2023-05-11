3355f81cc99b6c33e9e91a707f988d555f8a137a
Iuliia Dronova <jtyutereva@gmail.com>
2023-05-11 15:34:37 +0300

    Added missed test files

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -124,7 +125,8 @@ fun TipCalculatorLayout() {
     }
 }
 
-private fun calculateTip(
+@VisibleForTesting
+internal fun calculateTip(
     amount: Double,
     tipPercent: Double = 15.0,
     roundUp: Boolean
--- app/src/test/java/com/example/tipcalculator/TipCalculatorTests.kt
+++ app/src/test/java/com/example/tipcalculator/TipCalculatorTests.kt
@@ -1,17 +1,16 @@
 package com.example.tipcalculator
 
 import org.junit.Test
-
 import org.junit.Assert.*
+import java.text.NumberFormat
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+class TipCalculatorTests {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun calculateTip_20PercentNoRoundup() {
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount, tipPercent, false)
+        assertEquals(expectedTip, actualTip)
     }
 }
